3595f8eb40a9c82dbbf22b24942bfc922334384f
sandipv2 <--local>
2025-02-10 12:31:29 +0530

    tested

--- todoAppContext/src/App.jsx
+++ todoAppContext/src/App.jsx
@@ -31,19 +31,21 @@ function App() {
   };
 
   useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"))
+    const todos = JSON.parse(localStorage.getItem("todos"));
 
-    if(todos && todos.length > 0) {
-      setTodos(todos)
+    if (todos && todos.length > 0) {
+      setTodos(todos);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }, [todos])
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
