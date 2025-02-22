397e2d4e133f92fd12b3273e3d2207da0d100a74
sandipv2 <--local>
2025-02-22 17:08:18 +0530

    Added add folder and delete feature in File Explorer

--- fileExplorer/src/App.jsx
+++ fileExplorer/src/App.jsx
@@ -1,20 +1,48 @@
 import { useState } from "react";
 import json from "./data.json";
 
-function Comp({ data }) {
-  const [isExpanded, setIsExpanded] = useState({})
+function Comp({ data, addFolder, deleteFileFolder }) {
+  const [isExpanded, setIsExpanded] = useState({});
 
   const handleToggleClick = (item) => {
-    setIsExpanded(prev => ({...prev, [item.name]: !prev[item.name]}))
-  }
+    setIsExpanded((prev) => ({ ...prev, [item.name]: !prev[item.name] }));
+  };
 
   return (
     <div className="container">
       {data.map((item) => (
         <div key={item.id}>
-          {item.isFolder && <span className="toggleIcon" onClick={() => handleToggleClick(item)}>{isExpanded[item.name] ? "➖" : "➕"}</span>}
+          {item.isFolder && (
+            <span
+              className="toggleIcon"
+              onClick={() => handleToggleClick(item)}
+            >
+              {isExpanded[item.name] ? "➖" : "➕"}
+            </span>
+          )}
           {item.name}
-          {item.children && isExpanded[item.name] && <Comp data={item.children} />}
+          {item?.isFolder && (
+            <img
+              className="icon"
+              onClick={() => addFolder(item.id)}
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfMt43f5llkF5OgPwtIozkZk38jQu2r-3XCg&s"
+            />
+          )}
+          <img
+            onClick={() => deleteFileFolder(item.id)}
+            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            className="icon"
+            alt="delete-icon"
+          />
+
+          {/*Recursion below*/}
+          {item.children && isExpanded[item.name] && (
+            <Comp
+              data={item.children}
+              addFolder={addFolder}
+              deleteFileFolder={deleteFileFolder}
+            />
+          )}
         </div>
       ))}
     </div>
@@ -24,10 +52,55 @@ function Comp({ data }) {
 function App() {
   const [data, setData] = useState(json);
 
+  const addFolder = (itemId) => {
+    const name = prompt("Enter folder name: ");
+    const upDateList = (list) => {
+      return list.map((node) => {
+        if (node.id === itemId) {
+          return {
+            ...node,
+            children: [
+              ...node.children,
+              { id: Date.now(), name: name, isFolder: true, children: [] },
+            ],
+          };
+        }
+
+        if (node.children) {
+          return { ...node, children: upDateList(node.children) }; // Recursion
+        }
+
+        return node;
+      });
+    };
+
+    setData((prev) => upDateList(prev));
+  };
+
+  const deleteFileFolder = (itemId) => {
+    const deleteNode = (list) => {
+      return list
+      .filter((node) => node.id != itemId)
+      .map((node) => {
+        if(node.children) {
+          return {...node, children: deleteNode(node.children)}
+        }
+
+        return node;
+      })
+    }
+
+    setData(prev => deleteNode(prev))
+  };
+
   return (
     <div>
       <h1>File Explorer</h1>
-      <Comp data={data} />
+      <Comp
+        data={data}
+        addFolder={addFolder}
+        deleteFileFolder={deleteFileFolder}
+      />
     </div>
   );
 }
